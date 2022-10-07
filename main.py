2ff940d3c51a08339d01c0efc452b84c060f1e9f
Simon Marti <sim0mo@hotmail.com>
2022-10-07 10:32:53 +0200

    Fix typos in main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-def load_data(path_dataset):
-    """Load data."""
-    return data = np.genfromtxt(path_dataset, delimiter=",", skip_header=1])
+import numpy as np
+
+def load_features(path_dataset):
+    """Load all columns from data, droping the first two."""
+    return np.genfromtxt(path_dataset, delimiter=",", skip_header=1)[:,2:]
+
+def load_labels(path_dataset):
+    """Load second column of data."""
+    return np.genfromtxt(path_dataset, delimiter=",", skip_header=1, usecols=1)
 
 if __name__ == '__main__':
-    train = load_data("../data/train.csv")
-    test = load_data("../data/test.csv")
-    x_train = train[:,2:]
-    y_train = train[]
-    x_test = test[:,2:]
+    x_train = load_features("../data/train.csv")
+    y_train = load_labels("../data/train.csv")
+    x_test = load_features("../data/test.csv")
 
     print(x_train[:5,:])
     print(y_train[:5,:])
