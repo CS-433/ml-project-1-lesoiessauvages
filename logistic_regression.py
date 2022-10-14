0886a7fce7baad19cdd9bebcd4ae718758820dd6
Simon Marti <sim0mo@hotmail.com>
2022-10-14 10:26:51 +0200

    Remove unnecessary code

--- logistic_regression.py
+++ logistic_regression.py
@@ -11,7 +11,6 @@ def sigmoid(t):
     return 1.0 / (1 + np.exp(-t))
 
 
-
 def compute_loss(y, tx, w):
     """compute the cost by negative log likelihood.
 
@@ -23,11 +22,6 @@ def compute_loss(y, tx, w):
     Returns:
         a non-negative loss
 
-    >>> y = np.c_[[0., 1.]]
-    >>> tx = np.arange(4).reshape(2, 2)
-    >>> w = np.c_[[2., 3.]]
-    >>> round(calculate_loss(y, tx, w), 8)
-    3.04858961
     """
     assert y.shape[0] == tx.shape[0]
     assert tx.shape[1] == w.shape[0]
@@ -61,50 +55,3 @@ def compute_gradient(y, tx, w):
     grad = tx.T.dot(pred - y)
     loss = compute_loss(y, tx, w)
     return grad
-
-
-def learning_by_gradient_descent(y, tx, w, gamma):
-    """
-    Do one step of gradient descent using logistic regression. Return the loss and the updated w.
-
-    Args:
-        y:  shape=(N, 1)
-        tx: shape=(N, D)
-        w:  shape=(D, 1)
-        gamma: float
-
-    Returns:
-        loss: scalar number
-        w: shape=(D, 1)
-    """
-    
-    loss = calculate_loss(y, tx, w)
-    grad = calculate_gradient(y, tx, w)
-    w -= gamma * grad
-    return loss, w
-
-def logistic_regression_gradient_descent_demo(y, x):
-    # init parameters
-    max_iter = 10000
-    threshold = 1e-8
-    gamma = 0.01
-    losses = []
-
-    # build tx
-    tx = np.c_[np.ones((y.shape[0], 1)), x]
-    w = np.zeros((tx.shape[1], 1))
-
-    # start the logistic regression
-    for iter in range(max_iter):
-        # get loss and update w.
-        loss, w = learning_by_gradient_descent(y, tx, w, gamma)
-        # log info
-        if iter % 100 == 0:
-            print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
-        # converge criterion
-        losses.append(loss)
-        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
-            break
-    # visualization
-    visualization(y, x, mean_x, std_x, w, "classification_by_logistic_regression_gradient_descent", True)
-    print("loss={l}".format(l=calculate_loss(y, tx, w)))
